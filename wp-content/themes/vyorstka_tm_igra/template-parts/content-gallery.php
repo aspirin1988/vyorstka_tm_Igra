26ae2200c002805168146dde999d0acd62137c42
shari <sharipkanov@gmail.com>
2016-06-15 17:38:59 +0600

    das

--- wp-content/themes/vyorstka_tm_igra/template-parts/content-gallery.php
+++ wp-content/themes/vyorstka_tm_igra/template-parts/content-gallery.php
@@ -1,38 +1,28 @@
 <?php $obj=get_queried_object();?>
 
-<div class="row text-center fh5co-heading row-padded">
-	<div class="col-md-8 col-md-offset-2">
-		<h2 class="heading to-animate fadeInUp animated"><?=$obj->post_title; ?></h2>
-	</div>
-</div>
-
-<?php $gallery=pp_gallery_get($obj->ID);
-if($gallery):
-	?>
-	<div id="fh5co-gallery" data-section="gallery">
-		<div class="container">
-			<div class="row text-center fh5co-heading row-padded">
-				<div class="col-md-8 col-md-offset-2">
-					<a href="<?=get_permalink(212)?>"><h2 class="heading to-animate"><?=get_field('gallery',4)?></h2></a>
-				</div>
+<div id="fh5co-gallery" data-section="gallery">
+	<div class="container">
+		<div class="row text-center fh5co-heading row-padded">
+			<div class="col-md-8 col-md-offset-2">
+				<a href="<?=get_permalink(212)?>"><h2 class="heading to-animate"><?=get_field('gallery',4)?></h2></a>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<div id="gallery-pp-oo" class="">
-						<?php foreach($gallery as $galleryItem) : ?>
-							<a href="<?=$galleryItem->url?>">
-								<img alt="<?=$galleryItem->alt?>"
-									 src="<?=$galleryItem->url?>"
-									 data-image="<?=$galleryItem->url?>"
-									 style="display:none">
-							</a>
-						<?php endforeach; ?>
-					</div>
-					<div style="margin-top: 40px;"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div id="gallery-pp-oo" class="">
+					<?php foreach($gallery as $galleryItem) : ?>
+						<a href="<?=$galleryItem->url?>">
+							<img alt="<?=$galleryItem->alt?>"
+								 src="<?=$galleryItem->url?>"
+								 data-image="<?=$galleryItem->url?>"
+								 style="display:none">
+						</a>
+					<?php endforeach; ?>
 				</div>
+				<div style="margin-top: 40px;"></div>
 			</div>
 		</div>
 	</div>
-<?php endif; ?>
+</div>
 
 </div>
